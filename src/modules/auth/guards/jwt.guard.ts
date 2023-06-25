c93ccd0a361dd3c4a6547c00b9d924d400e2035f
BrunoUemura <bruno.h.uemura@outlook.com>
2023-06-25 17:51:35 -0300

    feat: jwt guard validation

---
+++ src/modules/auth/guards/jwt.guard.ts
@@ -0,0 +1,39 @@
+import {
+  BadGatewayException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      await this.authService.validateTokenSSO(token);
+    } catch (error) {
+      if (error.status === 401) {
+        throw new UnauthorizedException();
+      }
+
+      throw new BadGatewayException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
