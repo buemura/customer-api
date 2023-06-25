3e57b5eee32f32ed3e6ecae251b6444919566a9f
BrunoUemura <bruno.h.uemura@outlook.com>
2023-06-25 17:08:16 -0300

    test: adds authController generateToken

--- src/modules/auth/__tests__/auth.controller.spec.ts
+++ src/modules/auth/__tests__/auth.controller.spec.ts
@@ -27,6 +27,38 @@ describe('AuthController', () => {
     expect(sut).toBeDefined();
   });
 
+  describe('generateToken', () => {
+    it('should call generateTokenSSO and return the result', async () => {
+      const mockResult = {
+        status: 200,
+        data: {
+          access_token: 'string',
+          expires_in: 0,
+          refresh_expires_in: 0,
+          token_type: 'string',
+          id_token: 'string',
+          'not-before-policy': 0,
+          scope: 'string',
+        },
+      };
+      const generateTokenSSOSpy = jest
+        .spyOn(authService, 'generateTokenSSO')
+        .mockResolvedValueOnce(mockResult);
+
+      const mockBody = {
+        grant_type: 'string',
+        client_id: 'string',
+        client_secret: 'string',
+        username: 'string',
+        password: 'string',
+        scope: 'string',
+      };
+      const result = await sut.generateToken(mockBody);
+      expect(generateTokenSSOSpy).toHaveBeenCalledWith(mockBody);
+      expect(result).toEqual(mockResult);
+    });
+  });
+
   describe('getUserInfo', () => {
     it('should call validateTokenSSO and return the result', async () => {
       const mockResult = {
