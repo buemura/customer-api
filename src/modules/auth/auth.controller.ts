f110b3c3a8521c7fec11d1352d9c8d65ed6af785
BrunoUemura <bruno.h.uemura@outlook.com>
2023-06-25 17:08:34 -0300

    feat: adds authController generateToken

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,26 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { GetUserInfoDto } from './dtos';
+import { GenerateTokenDto, GetUserInfoDto } from './dtos';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('token')
+  @HttpCode(HttpStatus.OK)
+  async generateToken(@Body() body: GenerateTokenDto) {
+    return this.authService.generateTokenSSO(body);
+  }
 
   @Get('userinfo')
   async getUserInfo(@Body() body: GetUserInfoDto) {
