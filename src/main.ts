71e3531dba41ad4fc7a4c41795bed83b63d243cb
BrunoUemura <bruno.h.uemura@outlook.com>
2023-06-25 00:13:33 -0300

    feat: adds initial swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Stone Customer API')
+    .setDescription('The Stone Customer API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  await app.listen(port, () => console.log('Customer API running...'));
 }
+
 bootstrap();
