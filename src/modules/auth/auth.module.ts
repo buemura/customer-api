56d38ead87c5686f41c96e37e36aa9ed0630a87e
BrunoUemura <bruno.h.uemura@outlook.com>
2023-06-25 17:53:06 -0300

    feat: updates auth module controllers and exports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { SsoModule } from '@modules/sso/sso.module';
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [SsoModule],
+  controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
