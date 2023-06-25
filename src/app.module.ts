472a9428ff704ddd3c5baeff238f66fbaf64eb10
BrunoUemura <bruno.h.uemura@outlook.com>
2023-06-25 02:11:24 -0300

    feat: adds global configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 import { CustomersModule } from './modules/customers/customers.module';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    AuthModule,
+    CustomersModule,
+  ],
 })
 export class AppModule {}
