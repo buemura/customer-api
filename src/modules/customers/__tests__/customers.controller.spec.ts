a441265f2b38373e08807c2985e98631e5de08f5
BrunoUemura <bruno.h.uemura@outlook.com>
2023-06-25 01:37:10 -0300

    test: customer create controller success

--- src/modules/customers/__tests__/customers.controller.spec.ts
+++ src/modules/customers/__tests__/customers.controller.spec.ts
@@ -1,9 +1,11 @@
 import { CacheRepository } from '@modules/cache/cache.repository';
 import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { randomUUID } from 'crypto';
 
 import { CustomersController } from '../customers.controller';
 import { CustomersService } from '../customers.service';
+import { CreateCustomerDto } from '../dtos/create-customer.dto';
 import { Customer } from '../entities/customer.entity';
 
 describe('CustomersController', () => {
@@ -42,6 +44,26 @@ describe('CustomersController', () => {
       jest.spyOn(customersService, 'findById').mockResolvedValue(mockCustomer);
       const result = await sut.findById('1');
       expect(customersService.findById).toHaveBeenCalledWith('1');
+      expect(result).toEqual(mockCustomer);
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new customer', async () => {
+      const mockCustomer: Customer = {
+        id: randomUUID(),
+        document: 100,
+        name: 'Neil deGrasse Tyson',
+        createdAt: new Date(),
+      };
+      jest.spyOn(customersService, 'create').mockResolvedValue(mockCustomer);
+
+      const createCustomerDto: CreateCustomerDto = {
+        document: 100,
+        name: 'Neil deGrasse Tyson',
+      };
+      const result = await sut.create(createCustomerDto);
+      expect(customersService.create).toHaveBeenCalledWith(createCustomerDto);
       expect(result).toEqual(mockCustomer);
     });
   });
