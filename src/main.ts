a9cb10422c1a926c7842ab92611b3d7ee70e1f5b
BrunoUemura <bruno.h.uemura@outlook.com>
2023-06-25 19:12:47 -0300

    feat: adds validation pipe and logger interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { LoggerInterceptor } from '@infra/logger/logger.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggerInterceptor());
 
   const config = new DocumentBuilder()
     .setTitle('Stone Customer API')
