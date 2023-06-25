c3426fdbbac68afbf2e917e09488c48f9dc9fb6d
BrunoUemura <bruno.h.uemura@outlook.com>
2023-06-25 01:06:57 -0300

    test: customer findById return null when not found

--- src/modules/customers/__tests__/customers.service.spec.ts
+++ src/modules/customers/__tests__/customers.service.spec.ts
@@ -1,10 +1,11 @@
-import { CacheRepository } from '@modules/cache/cache.repository';
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { CacheRepository } from '@modules/cache/cache.repository';
 import { FakeRedisCacheRepository } from 'test/__mocks__/fake-redis-cache.repository';
 import { CustomersService } from '../customers.service';
 
 describe('CustomersService', () => {
-  let service: CustomersService;
+  let sut: CustomersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,10 +18,18 @@ describe('CustomersService', () => {
       ],
     }).compile();
 
-    service = module.get<CustomersService>(CustomersService);
+    sut = module.get<CustomersService>(CustomersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(sut).toBeDefined();
+  });
+
+  describe('findById', () => {
+    it('should return null when customer is not found', async () => {
+      const id = '809357ef-f537-4460-a71f-d859d01f74ec';
+      const customer = await sut.findById(id);
+      expect(customer).toBeNull();
+    });
   });
 });
